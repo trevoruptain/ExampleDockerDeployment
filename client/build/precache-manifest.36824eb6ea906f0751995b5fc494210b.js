d88f48796c171646a90df2cb714bae1d6eb3899d
trevoruptain <trevoruptain@gmail.com>
2020-01-31 18:22:49 -0600

    Fix server

---
+++ client/build/precache-manifest.36824eb6ea906f0751995b5fc494210b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "67d14706859ecec6218d",
+    "url": "/static/js/main.c59a3764.chunk.js"
+  },
+  {
+    "revision": "fc9a31ff2a9b1f9472aa",
+    "url": "/static/js/2.4ab07848.chunk.js"
+  },
+  {
+    "revision": "12d8431587647662416b946e3c3a7b03",
+    "url": "/index.html"
+  }
+];
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const models = require("./models");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -9,6 +10,13 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use(
   "/graphql",
