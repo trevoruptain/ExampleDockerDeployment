85da1dcd87b12217e7977fdd4e239ae57a4e9c6d
trevoruptain <trevoruptain@gmail.com>
2020-01-31 19:26:04 -0600

    update dockerfile

---
+++ client/build/precache-manifest.fbf584ba73c24808a1f436ea3379536a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cfbc143293931d61799529314086e90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a00645708149c4322232",
+    "url": "/static/js/2.aa9bd0d1.chunk.js"
+  },
+  {
+    "revision": "fb226702356a46ed44aa17eb4ca4076e",
+    "url": "/static/js/2.aa9bd0d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fdc1c93e6d83ef779ba",
+    "url": "/static/js/main.139a372c.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -14,7 +14,9 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    const indexPath = path.resolve(__dirname, "client", "build", "index.html");
+    console.log(indexPath);
+    res.sendFile(indexPath);
   });
 }
 
